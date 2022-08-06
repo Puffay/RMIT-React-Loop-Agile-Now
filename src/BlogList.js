08dd119330be740f8a04cca8addd89bdd15b37d1
Puffay <s3785133@student.rmit.edu.au>
2022-08-06 23:21:45 +1000

    fix

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,10 @@
 const BlogList = (props) => {
+    const blogs = props.blogs;
+    const title = props.title;
+
     return ( 
         <div className="home">
+            <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className='blog-preview' key={blog.id}>
                     <h3>{blog.title}</h3>
