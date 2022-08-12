662fbe07576355bb9fdb4bff20a8ee25592ce529
Puffay <s3785133@student.rmit.edu.au>
2022-08-13 03:52:42 +1000

    making navbar change when login

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,26 +9,49 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { Link, Router } from 'react-router-dom';
 import { fontWeight } from '@mui/system';
 
-const Navbar = () => {
+function Navbar(props) {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        <Link to='/' style={{ textDecoration: 'none', color: 'White', fontWeight:'Bold'}}>
+                    <Typography variant="h6" component="div" >
+                        <Link to='/' style={{ textDecoration: 'none', color: 'White', fontWeight: 'Bold' }}>
                             Loop Agile Now
                         </Link>
                     </Typography>
-                    <Button color="inherit" >
-                        <Link to="/login" style={{ textDecoration: 'none', color: 'White' }}>
-                            Login
-                        </Link>
-                    </Button>
-                    <Button color="inherit">
-                        <Link to="/signup" style={{ textDecoration: 'none', color: 'White' }}>
-                            Sign Up
-                        </Link>
-                    </Button>
+                    {props.username == null ?
+                        (<Button color="inherit" sx={{ ml: 'auto' }} >
+                            <Link to="/login" style={{ textDecoration: 'none', color: 'White' }} >
+                                Login
+                            </Link>
+                        </Button>)
+                        :
+                        (<Button color="inherit" sx={{ ml: '30px' }} >
+                            <Link to="/profile" style={{ textDecoration: 'none', color: 'White' }}>
+                                Profile
+                            </Link>
+                        </Button>)
+                    }
+                    {props.username != null &&
+                        (<Button color="inherit" >
+                            <Link to="/forum" style={{ textDecoration: 'none', color: 'White' }} >
+                                Forum
+                            </Link>
+                        </Button>)
+                    }
+                    {props.username == null ?
+                        (<Button color="inherit" >
+                            <Link to="/signup" style={{ textDecoration: 'none', color: 'White' }}>
+                                Sign Up
+                            </Link>
+                        </Button>)
+                        :
+                        (<Button color="inherit" sx={{ ml: 'auto' }}>
+                            <Link to="/logout" style={{ textDecoration: 'none', color: 'White' }}>
+                                Logout
+                            </Link>
+                        </Button>)
+                    }
                 </Toolbar>
             </AppBar>
         </Box>
