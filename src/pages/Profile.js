c6e2b82fbcbee1a864a825e630889f86ad9cce5e
Adrian93989 <adzero128@gmail.com>
2022-08-21 02:42:23 +1000

    fixed alot

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import Container from '@mui/system/Container';
 import { createTheme, Typography } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import styled from '@mui/system/styled';
+import { userContext } from '../App';
 
 const Profile = () => {
     const [name, setName] = useState('John Doe');
@@ -15,24 +17,51 @@ const Profile = () => {
         { title: 'Title', body: 'Description', author: 'Username3', id: 3 },
     ]);
 
+    const [user, setUser] = useContext(userContext);
+
+    const Item = styled('div')(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        borderColor: theme.palette.mode === 'dark' ? '#444d58' : '#ced7e0',
+        padding: theme.spacing(1),
+        borderRadius: '16px',
+        textAlign: 'center',
+    }));
+
     return (
         <Container>
             <Typography component='h1' variant='h7' align='center'>
-                Welcome ...username...
+                Welcome {user.name}
             </Typography>
-            <Box sx={{  border: 1, borderRadius: '16px', borderColor: 'white', boxShadow: 20, width: 600, height: 300, backgroundColor: 'primary.dark' }}>
-                <Container>
-                    <Avatar align="centre" sx={{ bgcolor: deepPurple[500], width: 200, height: 200 }}>
-                        <Typography variant="h3">
+            <Grid sx={{
+                border: 1,
+                borderRadius: '16px',
+                borderColor: 'black',
+                boxShadow: 20,
+                width: 600,
+                height: 300,
+                mx: 'auto'
+            }}>
+                <Item>
+                    <Avatar sx={{ bgcolor: deepPurple[500], width: 170, height: 170, mx:'auto'}}>
+                        <Typography variant="h3" >
                             NAME
                         </Typography>
                     </Avatar>
-                </Container>
-                <Container>
-                    <TextField name='name' type='text' label='Name' defaultValue="...name..." />
-                    <TextField name='email' type='text' label='Email' defaultValue="...email..." />
-                </Container>
-            </Box>
+                </Item>
+                <Item>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 2, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <TextField id="firstname" label="First Name" variant="filled" />
+                        <TextField id="lastname" label="Last Name" variant="filled" />
+                    </Box>
+                </Item>
+            </Grid>
         </Container >
     );
 }
